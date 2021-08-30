d2b33797e51c67d314a7908acd01dddd68c3ef95
deepakdabekar <deepakdabekar@nimapinfotech.com>
2021-08-30 10:39:55 +0530

    Added, adding product to cart functionality, getting total of all product and there price and update it in template

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from .models import Customer, Product, Order, OrderItem, ShippingAddress
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+
+# @admin.register(Customer)
+# class CustomerAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'id',
+#         'user',
+#         'name',
+#         'email'
+#     )
+
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'id',
+#         'name',
+#         'price',
+#         'digital',
+#         # 'product_quantity',
+#         'image',
+#     )
+#     list_editable = ('price',)
+
+# @admin.register(Order)
+# class OrderAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'id',
+#         'customer',
+#         'date_order',
+#         'complete',
+#         'transaction_id'
+#     )
+#     list_editable = ('customer', 'complete')
+
+# @admin.register(OrderItem)
+# class OrderItemAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'id',
+#         'product',
+#         'order',
+#         'quantity'
+#     )
+
+# @admin.register(ShippingAddress)
+# class ShippingAddressAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'id',
+#         'customer',
+#         'order',
+#         'address',
+#         'city',
+#         'state',
+#         'zipcode'
+#     )
